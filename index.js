04b93b95beef14f886a6caa7e04761621f531a30
Adam <adamjtew@gmail.com>
2016-01-15 12:55:28 -0700

    react init

---
+++ index.js
@@ -0,0 +1,13 @@
+var react = require('react')
+var render = require('react-dom').render
+
+var Root = React.createClass({
+	render: function() {
+		return <div> Hello World </div>;
+	}
+})
+
+render(
+	<Root />,
+	document.getElementById('root');
+)
