ef9cb71fc483667470e5f0e6be015fac993705c7
Adam <adamjtew@gmail.com>
2016-01-15 13:03:24 -0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var React = require('react')
 var reactDOM = require('react-dom')
 
-var Root = React.createClass({
-	render: function() {
-		return <div> Hello World </div>;
-	}
-})
-
 reactDOM.render(
 	<h1> Hello World </h1>,
 	document.getElementById('root');
